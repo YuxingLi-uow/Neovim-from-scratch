9fbae416a613e6b0e40d4c8c8c267bf39106ff34
YuxingLi-uow <liyuxing1210@gmail.com>
2025-04-29 16:22:43 +1000

    feature: set jj as quit shortcut

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -41,8 +41,7 @@ keymap("n", "<A-k>", ":m .-2<CR>==", opts)
 
 -- Insert --
 -- Press jk fast to exit insert mode 
-keymap("i", "jk", "<ESC>", opts)
-keymap("i", "kj", "<ESC>", opts)
+keymap("i", "jj", "<ESC>", opts)
 
 -- Visual --
 -- Stay in indent mode
